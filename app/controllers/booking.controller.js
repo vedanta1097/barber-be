e05b0fb268d7dcd20a2d09f43022e8abdd4996f2
vedanta1097 <vedantamaha@gmail.com>
2023-12-29 17:21:03 +0800

    add booking route and controller

---
+++ app/controllers/booking.controller.js
@@ -0,0 +1,76 @@
+const db = require('../models');
+const Booking = db.booking;
+
+exports.addBooking = (req, res) => {
+  try {
+    res.status(200).send({
+      message: 'Booking is successfully created'
+    })
+  } catch {
+    res.status(500).send({
+      message: 'Sorry, something went wrong on our end. Please try again later.'
+    });
+  }
+}
+
+exports.doneBooking = (req, res) => {
+  try {
+    res.status(200).send({
+      message: 'Booking is successfully completed'
+    })
+  } catch {
+    res.status(500).send({
+      message: 'Sorry, something went wrong on our end. Please try again later.'
+    });
+  }
+}
+
+exports.getBookings = (req, res) => {
+  try {
+    res.status(200).send({
+      message: 'Return list of bookings'
+    })
+  } catch {
+    res.status(500).send({
+      message: 'Sorry, something went wrong on our end. Please try again later.'
+    });
+  }
+}
+
+exports.getMyBookings = (req, res) => {
+  try {
+    res.status(200).send({
+      message: 'Return my list of bookings based on my token'
+    })
+  } catch {
+    res.status(500).send({
+      message: 'Sorry, something went wrong on our end. Please try again later.'
+    });
+  }
+}
+
+exports.getBookingsTime = (req, res) => {
+  try {
+    res.status(200).send({
+      message: 'Return list of time that already booked in that date'
+    })
+  } catch {
+    res.status(500).send({
+      message: 'Sorry, something went wrong on our end. Please try again later.'
+    });
+  }
+}
+
+
+exports.deleteBooking = (req, res) => {
+  try {
+    res.status(200).send({
+      message: 'Booking is successfully deleted'
+    })
+  } catch {
+    res.status(500).send({
+      message: 'Sorry, something went wrong on our end. Please try again later.'
+    });
+  }
+}
+
---
+++ app/routes/booking.routes.js
@@ -0,0 +1,48 @@
+const { authJwt } = require('../middleware');
+const controller = require('../controllers/booking.controller');
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      'Access-Control-Allow-Headers',
+      'x-access-token, Origin, Content-Type, Accept'
+    );
+    next();
+  });
+
+  app.post(
+    '/api/bookings',
+    [authJwt.verifyToken],
+    controller.addBooking
+  )
+
+  app.post(
+    '/api/bookings/:id/done',
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.doneBooking
+  )
+
+  app.get(
+    '/api/bookings',
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.getBookings
+  )
+
+  app.get(
+    '/api/bookings/me',
+    [authJwt.verifyToken],
+    controller.getBookings
+  )
+
+  app.get(
+    '/api/bookings/time',
+    [authJwt.verifyToken],
+    controller.getBookingsTime
+  )
+
+  app.delete(
+    '/api/bookings/:id',
+    [authJwt.verifyToken],
+    controller.deleteBooking
+  )
+};
