78d880ec578d972f50f37164c26854c53db25d22
BagusNata <bagusnata330@gmail.com>
2024-01-03 16:21:11 +0800

    add service feature

---
+++ app/controllers/service.controller.js
@@ -0,0 +1,86 @@
+const db = require("../models");
+const Service = db.service;
+
+exports.addService = async (req, res) => {
+  try {
+    await Service.create({
+      name: req.body.name,
+      price: req.body.price,
+      description: req.body.description,
+    });
+    res.status(200).send({
+      message: "Service is successfully added.",
+    });
+  } catch {
+    res.status(500).send({
+      message:
+        "Sorry, something went wrong on our end. Please try again later.",
+    });
+  }
+};
+
+exports.updateService = async (req, res) => {
+  try {
+    const service = await Service.findByPk(req.params.id);
+    if (!service) {
+      return res.status(404).send({ message: "Service Not found." });
+    }
+    await Service.update(
+      {
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).send({
+      message: `Service ${service.name} is successfully updated.`,
+    });
+  } catch {
+    res.status(500).send({
+      message:
+        "Sorry, something went wrong on our end. Please try again later.",
+    });
+  }
+};
+
+exports.getServices = async (req, res) => {
+  try {
+    const services = await Service.findAll();
+    res.status(200).send(services);
+  } catch {
+    res.status(500).send({
+      message:
+        "Sorry, something went wrong on our end. Please try again later.",
+    });
+  }
+};
+
+exports.deleteService = async (req, res) => {
+  try {
+    const service = await Service.findByPk(req.params.id);
+    if (!service) {
+      return res.status(404).send({ message: "Service Not found." });
+    }
+    await Service.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).send({
+      message: "Service is successfully deleted",
+    });
+  } catch {
+    res.status(500).send({
+      message:
+        "Sorry, something went wrong on our end. Please try again later.",
+    });
+  }
+};
+
+
+
--- app/models/service.model.js
+++ app/models/service.model.js
@@ -1,11 +1,14 @@
 module.exports = (sequelize, Sequelize) => {
-  const Service = sequelize.define('services', {
+  const Service = sequelize.define("services", {
     name: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
     },
     price: {
-      type: Sequelize.INTEGER
-    }
+      type: Sequelize.INTEGER,
+    },
+    description: {
+      type: Sequelize.STRING,
+    },
   });
 
   return Service;
